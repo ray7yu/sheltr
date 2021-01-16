bbee2e9fd212448b36d470b55d061889b8885a22
Leon <kwan.leon@gmail.com>
2021-01-16 16:54:54 -0500

    multiple pins not working

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -4,20 +4,27 @@ import API_KEY from '../../config'
 import LocationPin from './LocationPin'
 import './map.css'
 
-const Map = ({ location, zoomLevel }) => (
+const Map = ({ center, locations, zoomLevel }) => (
   <div className="map">
     <h2 className="map-h2">Come Visit Us At Our Campus</h2>
     <div className="google-map">
       <GoogleMapReact
         bootstrapURLKeys={{ key: API_KEY }}
-        defaultCenter={location}
+        defaultCenter={center}
         defaultZoom={zoomLevel}
       >
-        <LocationPin
-          lat={location.lat}
-          lng={location.lng}
-          text={location.address}
-        />
+        {
+          locations.map((location)=>{
+            return(
+              <LocationPin
+                lat={location.geometry.x}
+                lng={location.geometry.y}
+                text={location.STD_ADDR}
+              />
+            )
+          })
+        }
+        
       </GoogleMapReact>
     </div>
   </div>
