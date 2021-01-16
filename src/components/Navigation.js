3a73d838aa1fc3f9b5c1a0730c9e3f534539fc68
Leon <kwan.leon@gmail.com>
2021-01-16 13:58:46 -0500

    merge

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,6 @@ import './Navigation.css';
 import { Form, FormControl, Nav, Navbar, Button, Container } from 'react-bootstrap';
 function Navigation() {
   return (
-<<<<<<< HEAD
-    
-      <Navbar sticky="top" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-=======
     <Navbar sticky="top" bg="dark" variant="dark" expand="sm">
       <Navbar.Brand href="#home"> 
       <img
@@ -22,7 +17,6 @@ function Navigation() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
       <Navbar.Collapse id="basic-navbar-nav">
->>>>>>> origin/navigation
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">About</Nav.Link>
