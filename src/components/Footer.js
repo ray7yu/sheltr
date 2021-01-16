ad039566e5b92d6e17b3653826426f5d5e5cbf93
Raymond <wrg7yu@gmail.com>
2021-01-16 18:14:05 -0500

    Add footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Footer.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <div className="Footer">
-        <img src="/sheltr-white.png" alt="logo" className="Logo"/>
+        Developed by Leon Kwan, Michael Yu, Raymond Yu, and Venkat Pamulapati
+        <i class="fab fa-github"></i>
+        <div className="Social">
+            <FontAwesomeIcon icon={faGithub} className="Github" onClick={() => window.open("https://github.com/ray7yu/sheltr", "_blank")}/>
+        </div>
     </div>
   );
 }
